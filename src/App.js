e77cf76a010d46265f110cd22108e8fb9356c5eb
vgnsh1999 <vigneshvarathan@gmail.com>
2023-08-27 09:48:50 +0530

    Practicing React by adding user

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import AddUser from "./componenets/Users/AddUser";
+import UsersList from "./componenets/Users/UsersList";
+
+const App = () => {
+  const [usersList, setUsersList] = useState([]);
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [...prevUsersList, { name: uName, age: uAge ,id:Math.random().toString() }];
+    });
+  };
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </div>
+  );
+};
+
+export default App;
