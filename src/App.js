7b1d7bca1382da1353fffb82173459acd558f616
vgnsh1999 <vigneshvarathan@gmail.com>
2023-08-27 10:34:34 +0530

    Added built-in wrapper i.e. Fragment

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ const App = () => {
     });
   };
   return (
-    <div>
+    <>
       <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList} />
-    </div>
+    </>
   );
 };
 
